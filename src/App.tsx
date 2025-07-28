111be5365475a27bf7a9b0faa6668730f16dfb5c
paras-patankar <patankarparas@gmail.com>
2025-07-28 17:51:18 +0530

    newChanges

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,37 @@ import { ThemeProvider } from "./context/themeprovider";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPage from "./pages/CityPage";
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
+  
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
+  defaultOptions: {
+    queries: {
       staleTime: 5 * 60 * 1000, //5 minutes
       gcTime: 10 * 60 * 1000, //10 minutes
       retry: false,
       refetchOnWindowFocus: false,
-    }
-  }
+    },
+  },
 });
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          
-          <Routes>
-            <Route path='/' element={<WeatherDashboard/>} />
-            <Route path='/city/:cityName' element={<CityPage/>} />
-           
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
